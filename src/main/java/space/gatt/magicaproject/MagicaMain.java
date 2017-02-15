bcd65d9aceeba47fbe80e4b3319ee97b4b21dbfb
Gatt <zacharygatt@me.com>
2017-02-15 22:44:15 +1100

    Pew

---
+++ src/main/java/space/gatt/magicaproject/MagicaMain.java
@@ -0,0 +1,14 @@
+package space.gatt.magicaproject;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagicaMain extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public void onEnable() {
+	}
+}
