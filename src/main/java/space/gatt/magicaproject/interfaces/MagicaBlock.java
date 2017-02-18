b609d8e88174ace4e58edd4429911cf9fe92b041
Gatt <zacharygatt@me.com>
2017-02-18 23:51:57 +1100

    Optimised imports

--- src/main/java/space/gatt/magicaproject/interfaces/MagicaBlock.java
+++ src/main/java/space/gatt/magicaproject/interfaces/MagicaBlock.java
@@ -1,10 +1,6 @@
 package space.gatt.magicaproject.interfaces;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.List;
 
 public interface MagicaBlock {
 
